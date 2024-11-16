5d8407c6c505a14d1a83a2a199226882ecaa0c29
real-levi-private <otterbach.levi@gmail.com>
2024-11-16 09:19:22 +0100

    refactor some codesmells

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SSP
+
+## TODO
+
+### 15.11 - 22.11
+
+ - [ ] change sspspace py file with library
+ - [ ] add vocab 2 with combined vectors of shape type color
+ - [ ] generate all ssps only once at the start
+ - [ ] make statistics for different length_scales
+ - [ ] add prints for all the other attributes
+ - [ ] make code a bit cleaner
+
--- src/SSP.py
+++ src/SSP.py
@@ -8,7 +8,7 @@ import nengo_spa as spa
 from matplotlib.patches import Rectangle, Polygon, Circle
 from webcolors import rgb_to_name
 
-from sspspace import SPSpace, SSPSpace, HexagonalSSPSpace
+from sspspace import HexagonalSSPSpace
 
 SHAPES = [
     # walls
@@ -33,57 +33,51 @@ ENTITIES = [
 ]
 
 
-# CLASS_LST = ['GREEN', 'PURPLE', 'YELLOW', 'CIRCLE', 'SQUARE', 'TRIANGLE']
-
 class SSP:
 
     def __init__(self,
                  grid_objects_param,
                  ):
         self.grid_objects = grid_objects_param
-        SSP_DIM = 1015
-        RES_X, RES_Y = 200, 200
-        RNG = np.random.RandomState(42)
+        self.SSP_DIM = 1015
+        self.RES_X, self.RES_Y = 200, 200
 
-        vocab = spa.Vocabulary(dimensions=SSP_DIM, pointer_gen=RNG)
+        rng = np.random.RandomState(42)
+        vocab = spa.Vocabulary(dimensions=self.SSP_DIM, pointer_gen=rng)
 
         # Add semantic pointers for each shape to the vocabulary
         for i, shape_name in enumerate(SHAPES):
-            vector = vocab.algebra.create_vector(SSP_DIM, properties={"positive", "unitary"})
+            vector = vocab.algebra.create_vector(self.SSP_DIM, properties={"positive", "unitary"})
             vocab.add(f"{shape_name.upper().replace(' ', '')}", vector)
 
         # Add semantic pointers for each entity to the vocabulary
         for i, entity_name in enumerate(ENTITIES):
-            vector = vocab.algebra.create_vector(SSP_DIM, properties={"positive", "unitary"})
+            vector = vocab.algebra.create_vector(self.SSP_DIM, properties={"positive", "unitary"})
             vocab.add(f"{entity_name.upper().replace(' ', '')}", vector)
 
         # Add semantic pointers for each color to the vocabulary
         for obj in self.grid_objects:
-            vector = vocab.algebra.create_vector(SSP_DIM, properties={"positive", "unitary"})
+            vector = vocab.algebra.create_vector(self.SSP_DIM, properties={"positive", "unitary"})
             color_name = rgb_to_name(obj.name['color']).upper().replace(' ', '')
             if vocab.get(color_name) is None:
                 vocab.add(f"{color_name.upper()}", vector)
 
         # Create SSP spaces for xy coordinates and labels
-        ssp_space = HexagonalSSPSpace(domain_dim=2, ssp_dim=SSP_DIM, length_scale=5,
-                                      domain_bounds=np.array([[0, RES_X], [0, RES_Y]]))
+        ssp_space = HexagonalSSPSpace(domain_dim=2, ssp_dim=self.SSP_DIM, length_scale=5,
+                                      domain_bounds=np.array([[0, self.RES_X], [0, self.RES_Y]]))
 
         # Generate xy coordinates
-        x_coords, y_coords = torch.meshgrid(torch.arange(0, RES_X), torch.arange(0, RES_Y),
+        x_coords, y_coords = torch.meshgrid(torch.arange(0, self.RES_X), torch.arange(0, self.RES_Y),
                                             indexing='ij')
         coords = np.stack((x_coords.flatten(), y_coords.flatten()), axis=-1)
         ssp_grid = ssp_space.encode(coords)
-        ssp_grid = ssp_grid.reshape((RES_X, RES_Y, -1))
+        ssp_grid = ssp_grid.reshape((self.RES_X, self.RES_Y, -1))
 
         print(f'Generated SSP grid: {ssp_grid.shape}')
 
-        # example visualization
-        # self._print_image(RES_X, RES_Y)
-
         # combine all object ssps
-
         ## init empty environment
-        global_env_ssp = np.zeros(SSP_DIM)
+        global_env_ssp = np.zeros(self.SSP_DIM)
 
         ## bind and add alls object ssps
         for obj in self.grid_objects:
@@ -101,12 +95,15 @@ class SSP:
             object_ssp = ssp_space.bind(ssp_space.bind(ssp_space.bind(type_ssp, shape_ssp), color_ssp), position_ssp)
             global_env_ssp += object_ssp.squeeze()
 
+        # example visualization of image
+        self._print_image()
         # example extracted visualization
-        # self._print_extracted_image(RES_X, RES_Y, SSP_DIM, global_env_ssp, ssp_grid, ssp_space, vocab)
+        self._print_extracted_image(global_env_ssp, ssp_grid, ssp_space, vocab)
 
         self.vector = "idk yet"
 
-    def _print_extracted_image(self, RES_X_param, RES_Y_param, SSP_DIM_param, global_env_ssp_param, ssp_grid_param, ssp_space_param, vocab_param):
+    def _print_extracted_image(self, ssp_dim_param, global_env_ssp_param, ssp_grid_param,
+                               ssp_space_param, vocab_param):
         for obj in self.grid_objects:
             obj_type = get_value_from_string(obj.name['type'])
             if obj_type == 'agent':
@@ -124,10 +121,10 @@ class SSP:
 
                 # get similarity map of label with all locations by binding with inverse ssp
                 out = ssp_space_param.bind(global_env_ssp_param, inv_ssp)
-                sims = out @ ssp_grid_param.reshape((-1, SSP_DIM_param)).T
+                sims = out @ ssp_grid_param.reshape((-1, ssp_dim_param)).T
 
                 # decode location = point with maximum similarity to label
-                sims_map = sims.reshape((RES_X_param, RES_Y_param))
+                sims_map = sims.reshape((self.RES_X, self.RES_Y))
 
                 pred_loc = np.array(np.unravel_index(np.argmax(sims_map), sims_map.shape))
                 x_predicted = pred_loc[1] - 10
@@ -149,7 +146,7 @@ class SSP:
                 print(
                     f'{obj_type.upper()} predicted location: {x_predicted, y_predicted}, correct location: {x_correct, y_correct}, {color} distance between: {distance} {RESET}')
 
-                plt.imshow(sims_map, extent=(0, RES_X_param, 0, RES_Y_param))
+                plt.imshow(sims_map, extent=(0, self.RES_X, 0, self.RES_Y))
                 plt.xticks([0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200])
                 plt.yticks([0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200])
 
@@ -165,19 +162,18 @@ class SSP:
 
                 plt.show()
 
-    def _print_image(self, res_x_param, res_y_param):
-        # example visualisation--------------------------------------- remove
+    def _print_image(self):
         fig, ax = plt.subplots()
-        global_grid = np.zeros((res_x_param, res_y_param))
+        global_grid = np.zeros((self.RES_X, self.RES_Y))
         plt.imshow(global_grid, extent=[0, 200, 0, 200], cmap='gray')  # Setze extent auf [0, 200, 0, 200]
-        plt.xticks([0, 20, 40, 60, 80, 100, 120, 140, 160, 180, res_x_param])  # x-Achse bei 0, 100 und 200
-        plt.yticks([0, 20, 40, 60, 80, 100, 120, 140, 160, 180, res_y_param])  # y-Achse bei 0, 100 und 200
+        plt.xticks([0, 20, 40, 60, 80, 100, 120, 140, 160, 180, self.RES_X])  # x-Achse bei 0, 100 und 200
+        plt.yticks([0, 20, 40, 60, 80, 100, 120, 140, 160, 180, self.RES_Y])  # y-Achse bei 0, 100 und 200
         # Gitterlinien
         grid_color = (.3, .3, .3, .5)
         grid_spacing = 20  # Abstand der Gitterlinien in Pixeln
-        for i in range(0, res_x_param, grid_spacing):  # Setze Gitterlinien im Abstand von 20
-            plt.hlines(i + grid_spacing, 0, res_y_param, color=grid_color, linewidth=1.0)
-            plt.vlines(i + grid_spacing, 0, res_x_param, color=grid_color, linewidth=1.0)
+        for i in range(0, self.RES_X, grid_spacing):  # Setze Gitterlinien im Abstand von 20
+            plt.hlines(i + grid_spacing, 0, self.RES_Y, color=grid_color, linewidth=1.0)
+            plt.vlines(i + grid_spacing, 0, self.RES_X, color=grid_color, linewidth=1.0)
         # add elements
         for object_element in self.grid_objects:
             x_pos_nulled = object_element.x - 10 + 20
@@ -185,19 +181,19 @@ class SSP:
             # walls
             if get_value_from_string(object_element.name['type']) == "walls":
                 ax.add_patch(
-                    Rectangle((y_pos_nulled, res_x_param - x_pos_nulled), width=20, height=20, color='gray',
+                    Rectangle((y_pos_nulled, self.RES_X - x_pos_nulled), width=20, height=20, color='gray',
                               zorder=3))
             # agent
             if get_value_from_string(object_element.name['type']) == "agent":
                 ax.add_patch(Polygon(
-                    [[y_pos_nulled, res_x_param - x_pos_nulled], [y_pos_nulled, res_x_param - x_pos_nulled + 20],
-                     [y_pos_nulled + 20, res_x_param - x_pos_nulled + 10]], closed=True, color='r', zorder=5))
+                    [[y_pos_nulled, self.RES_X - x_pos_nulled], [y_pos_nulled, self.RES_X - x_pos_nulled + 20],
+                     [y_pos_nulled + 20, self.RES_X - x_pos_nulled + 10]], closed=True, color='r', zorder=5))
             # objects
             if get_value_from_string(object_element.name['type']) == "objects":
-                ax.add_patch(Circle((y_pos_nulled, res_x_param - x_pos_nulled), radius=10, color='g', zorder=5))
+                ax.add_patch(Circle((y_pos_nulled, self.RES_X - x_pos_nulled), radius=10, color='g', zorder=5))
             if get_value_from_string(object_element.name['type']) == "home_entity":
                 ax.add_patch(
-                    Rectangle((y_pos_nulled, res_x_param - x_pos_nulled), width=15, height=15, color='m', zorder=5))
+                    Rectangle((y_pos_nulled, self.RES_X - x_pos_nulled), width=15, height=15, color='m', zorder=5))
         plt.title('Global Environment')
         plt.show()
 
